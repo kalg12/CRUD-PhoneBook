8c74da23b801a6d9dceeb204036e2a32236ccbea
kalg12 <78965771+kalg12@users.noreply.github.com>
2021-11-10 09:17:03 -0600

    js updates

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+function myData(){
+
+    const dataContacts = [
+
+        nombre = document.getElementById("name").value,
+        lastname = document.getElementById("lastname").value,
+        company = document.getElementById("company").value,
+        phone = document.getElementById("phone").value,
+        email = document.getElementById("email").value,
+        coments = document.getElementById("coments").value,
+    ]
+        console.log(dataContacts);
+
+}
+
+
 
